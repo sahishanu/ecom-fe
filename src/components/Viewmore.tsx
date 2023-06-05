b0f3a995c43da79d37706ecb28401a861968e123
sahishanu <sahiscord@gmail.com>
2023-06-05 17:31:23 +0530

    product details page added

--- src/components/Viewmore.tsx
+++ src/components/Viewmore.tsx
@@ -3,14 +3,27 @@ import React from "react";
 import { AiOutlineArrowRight } from "react-icons/ai"
 interface ViewmoreProps{
     text:string;
-    path?:string
+    path?:string;
+    title:string;
+    subtitle?:string;
+
+}
+interface ProductsProps{
+    title:string;
+    
 }
 
-export const Viewmore:React.FC<ViewmoreProps>=({text,path})=>{
+
+export const Viewmore:React.FC<ViewmoreProps>=({text,path,title})=>{
 const pathUrl:string="/"+path;
+const propsToSend= {
+    title: title,
+  };
+  console.log(propsToSend);
 return (
+
     <div>
-    <Link href={pathUrl}>
+    <Link href={{pathname:pathUrl, query:propsToSend}}>
     <button className="flex items-center hover:text-[#F9FBFC] hover:bg-[#171828]  border-2 font-semibold text-xs uppercase border-[#171828] w-fit px-4 py-3  ">
        <span > {text}</span>
        <span className="ml-2" > <AiOutlineArrowRight className="font-bold" size={20}/></span>
--- src/pages/productlisting.tsx
+++ src/pages/productlisting.tsx
@@ -4,57 +4,64 @@ import Navbar from "@/components/Navbar"
 import { Product } from "@/components/Proctuctview";
 import { Sectionheader } from "@/components/Sectionheader";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react"
 import {} from "react-icons"
 import { BsFillArrowUpCircleFill } from "react-icons/bs";
 
+interface Props{
+    title:string;
+    subtitle?:string;
+}
+
 const productlisting:React.FC<{}>=()=>{
 
-    
-    
+    const router =useRouter();
+    const title =router.query.title as string;
+    console.log(router.query);
     return (
         <div>
             <Navbar/>
             <span className="uppercase text-xs px-3  "><Link className="text-blue-700" href={"/"}>home</Link>&gt; Explore Products</span>
             
-            <Sectionheader  title={"Explore Products"}/>
+            <Sectionheader  title={title}/>
             <div>
             <div className="grid grid-cols-4 gap-4  mt-10 w-[85%] mx-auto">
             <div className="   ">
-    <Product name={"xyz"} cost={0} imgUrl={"./images/productsm/shirt.jpg"}/>  
+    <Product name={"xyz"} cost={0} imgUrl={"./images/productsm/shirt.jpg"} path={"/productdetails"}/>  
     </div>
     <div className="   ">
-    <Product name={"xyz"} cost={0} imgUrl={"./images/productsm/shirt.jpg"}/>  
+    <Product name={"xyz"} cost={0} imgUrl={"./images/productsm/shirt.jpg"} path={""}/>  
     </div>
     <div className="   ">
-    <Product name={"xyz"} cost={0} imgUrl={"./images/productsm/shirt.jpg"}/>  
+    <Product name={"xyz"} cost={0} imgUrl={"./images/productsm/shirt.jpg"} path={""}/>  
     </div>
     <div className="   ">
-    <Product name={"xyz"} cost={0} imgUrl={"./images/productsm/shirt.jpg"}/>  
+    <Product name={"xyz"} cost={0} imgUrl={"./images/productsm/shirt.jpg"} path={""}/>  
     </div>
     <div className="   ">
-    <Product name={"xyz"} cost={0} imgUrl={"./images/productsm/shirt.jpg"}/>  
+    <Product name={"xyz"} cost={0} imgUrl={"./images/productsm/shirt.jpg"} path={""}/>  
     </div>
     <div className="   ">
-    <Product name={"xyz"} cost={0} imgUrl={"./images/productsm/shirt.jpg"}/>  
+    <Product name={"xyz"} cost={0} imgUrl={"./images/productsm/shirt.jpg"} path={""}/>  
     </div>
     <div className="   ">
-    <Product name={"xyz"} cost={0} imgUrl={"./images/productsm/shirt.jpg"}/>  
+    <Product name={"xyz"} cost={0} imgUrl={"./images/productsm/shirt.jpg"} path={""}/>  
     </div>
     <div className="   ">
-    <Product name={"xyz"} cost={0} imgUrl={"./images/productsm/shirt.jpg"}/>  
+    <Product name={"xyz"} cost={0} imgUrl={"./images/productsm/shirt.jpg"} path={""}/>  
     </div>
     <div className="   ">
-    <Product name={"xyz"} cost={0} imgUrl={"./images/productsm/shirt.jpg"}/>  
+    <Product name={"xyz"} cost={0} imgUrl={"./images/productsm/shirt.jpg"} path={""}/>  
     </div>
     <div className="   ">
-    <Product name={"xyz"} cost={0} imgUrl={"./images/productsm/shirt.jpg"}/>  
+    <Product name={"xyz"} cost={0} imgUrl={"./images/productsm/shirt.jpg"} path={""}/>  
     </div>
     <div className="   ">
-    <Product name={"xyz"} cost={0} imgUrl={"./images/productsm/shirt.jpg"}/>  
+    <Product name={"xyz"} cost={0} imgUrl={"./images/productsm/shirt.jpg"} path={""}/>  
     </div>
     <div className="   ">
-    <Product name={"xyz"} cost={0} imgUrl={"./images/productsm/shirt.jpg"}/>  
+    <Product name={"xyz"} cost={0} imgUrl={"./images/productsm/shirt.jpg"} path={""}/>  
     </div>
     </div>
                 

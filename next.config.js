90cf20893f65a10add3f74ae0f9edab33af0a58a
sahishanu <sahiscord@gmail.com>
2023-06-10 21:49:06 +0530

    adding image domain in next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.dummyjson.com',
+        port: '',
+        pathname: '**',
+      },
+    ],
+  },
+
 }
 
 module.exports = nextConfig
